100bb5304a771936c593d180b90e9ac8ec668b96
Matheus Nunes <matheus.o.nunes@gmail.com>
2017-07-23 21:52:58 -0300

    Undo removal of extra command for kubectl permission inside git hook

--- save_secrets.sh
+++ save_secrets.sh
@@ -9,6 +9,7 @@ printf "DOCKER_API_VERSION=$DOCKER_API_VERSION\n" >> /set_env_vars.sh
 printf "CONTAINER_REGISTRY_SA='$CONTAINER_REGISTRY_SA'\n" >> /set_env_vars.sh
 printf "CLUSTER='$CLUSTER'\n" >> /set_env_vars.sh
 printf "ZONE='$ZONE'\n" >> /set_env_vars.sh
+echo $GKE_SA > /gke_sa.json
 chmod +x /set_env_vars.sh
 
 /usr/sbin/sshd -D
