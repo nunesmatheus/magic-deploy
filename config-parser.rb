1472b827f6098377efc5a2a90cbe820fe248aaca
Matheus Nunes <matheus.o.nunes@gmail.com>
2017-08-09 19:54:50 -0300

    Support custom deploy command and before command

--- config-parser.rb
+++ config-parser.rb
@@ -31,12 +31,28 @@ begin
     quit "'application' key on magic-deploy.yml not found!"
   end
 
+  before_deploy_command = app_config['before_deploy_command']
+  if before_deploy_command.is_a?(Array) && before_deploy_command.any?
+    before_deploy_command = before_deploy_command.join ' && '
+  else
+    before_deploy_command = ''
+  end
+
+  deploy_command = app_config['deploy_command']
+  if deploy_command.is_a?(Array) && deploy_command.any?
+    deploy_command = deploy_command.join ' && '
+  else
+    deploy_command = 'kubectl set image deployment $APPLICATION_NAME $APPLICATION_NAME=$APPLICATION_TAG'
+  end
+
   app_directory = "/apps/#{application}"
   Dir.mkdir app_directory unless Dir.exists?(app_directory)
 
   File.open("set_env_vars.sh", 'w') do |f|
     f.puts File.read('/set_env_vars.sh')
     f.puts "APPLICATION_NAME=#{application}"
+    f.puts "BEFORE_DEPLOY_COMMAND=#{before_deploy_command}"
+    f.puts "DEPLOY_COMMAND=#{deploy_command}"
   end
 rescue Exception => e
   quit "ERROR #{e.message}"
